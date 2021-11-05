0b9134cafb573995ccbc1602fcb11ade69bc4b8f
Julien Pignatel <pignatel.julien@gmail.com>
2021-11-05 15:24:24 +0100

    suppression de l'entrée @Service sur les entités

--- src/main/java/com/mycompany/newbie/service/ReservationService.java
+++ src/main/java/com/mycompany/newbie/service/ReservationService.java
@@ -1,48 +1,99 @@
 package com.mycompany.newbie.service;
 
+import com.mycompany.newbie.domain.Reservation;
+import com.mycompany.newbie.repository.ReservationRepository;
 import com.mycompany.newbie.service.dto.ReservationDTO;
+import com.mycompany.newbie.service.mapper.ReservationMapper;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Service Interface for managing {@link com.mycompany.newbie.domain.Reservation}.
+ * Service Implementation for managing {@link Reservation}.
  */
-public interface ReservationService {
+@Service
+@Transactional
+public class ReservationService {
+
+    private final Logger log = LoggerFactory.getLogger(ReservationService.class);
+
+    private final ReservationRepository reservationRepository;
+
+    private final ReservationMapper reservationMapper;
+
+    public ReservationService(ReservationRepository reservationRepository, ReservationMapper reservationMapper) {
+        this.reservationRepository = reservationRepository;
+        this.reservationMapper = reservationMapper;
+    }
+
     /**
      * Save a reservation.
      *
      * @param reservationDTO the entity to save.
      * @return the persisted entity.
      */
-    ReservationDTO save(ReservationDTO reservationDTO);
+    public ReservationDTO save(ReservationDTO reservationDTO) {
+        log.debug("Request to save Reservation : {}", reservationDTO);
+        Reservation reservation = reservationMapper.toEntity(reservationDTO);
+        reservation = reservationRepository.save(reservation);
+        return reservationMapper.toDto(reservation);
+    }
 
     /**
-     * Partially updates a reservation.
+     * Partially update a reservation.
      *
      * @param reservationDTO the entity to update partially.
      * @return the persisted entity.
      */
-    Optional<ReservationDTO> partialUpdate(ReservationDTO reservationDTO);
+    public Optional<ReservationDTO> partialUpdate(ReservationDTO reservationDTO) {
+        log.debug("Request to partially update Reservation : {}", reservationDTO);
+
+        return reservationRepository
+            .findById(reservationDTO.getId())
+            .map(existingReservation -> {
+                reservationMapper.partialUpdate(existingReservation, reservationDTO);
+
+                return existingReservation;
+            })
+            .map(reservationRepository::save)
+            .map(reservationMapper::toDto);
+    }
 
     /**
      * Get all the reservations.
      *
      * @return the list of entities.
      */
-    List<ReservationDTO> findAll();
+    @Transactional(readOnly = true)
+    public List<ReservationDTO> findAll() {
+        log.debug("Request to get all Reservations");
+        return reservationRepository.findAll().stream().map(reservationMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+    }
 
     /**
-     * Get the "id" reservation.
+     * Get one reservation by id.
      *
      * @param id the id of the entity.
      * @return the entity.
      */
-    Optional<ReservationDTO> findOne(Long id);
+    @Transactional(readOnly = true)
+    public Optional<ReservationDTO> findOne(Long id) {
+        log.debug("Request to get Reservation : {}", id);
+        return reservationRepository.findById(id).map(reservationMapper::toDto);
+    }
 
     /**
-     * Delete the "id" reservation.
+     * Delete the reservation by id.
      *
      * @param id the id of the entity.
      */
-    void delete(Long id);
+    public void delete(Long id) {
+        log.debug("Request to delete Reservation : {}", id);
+        reservationRepository.deleteById(id);
+    }
 }
